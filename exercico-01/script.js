d9a5e5bbe70caaed5bcadc12f914fc3f9c2cbf32
Lucas Pina <ti.lucaspina@gmail.com>
2022-01-12 13:42:35 -0300

    estudo js

--- exercico-01/script.js
+++ exercico-01/script.js
@@ -8,12 +8,41 @@ ou
 "Olá [nome], você é MENOR de idade"  
 */
 
-let nome = prompt(`Digite seu nome`);
-let idade = prompt(`Digite sua idade`);
+/* 
+2 - Modifique o exercício anterior para exibir uma mensagem personalizada caso o nome da pessoa seja "Thomas Anderson", exibir: 
+
+"Olá [nome], você é MAIOR de idade. Você é personagem do filme The Matrix!". 
+
+Mantenha a mensagem condizente com a idade informada.
+*/
+
+​/* 3 - Modifique o exercício anterior para pedir os dados do usuário somente depois dele clicar em um botão escrito "Entrar". 
+
+Faça a mensagem de saudação ser exibida em um H1, e a mensagem personalizada em um H2.​ */
+
+let titulo = document.querySelector('#titulo');
+let titulo = document.querySelector('#subtitulo');
 
 
-if (idade >= 18) {
-  document.write (`Olá ${nome}, você é Maior de idade!`)
-} else {
-  document.write(`Olá ${nome}, você é Menor de idade!`)
+function entrar() {
+  let nome = prompt(`Digite seu nome`);
+  let idade = prompt(`Digite sua idade`);
+  
+  let mensagem;
+  let mensagem2;
+  
+  
+  if (idade >= 18) {
+    mensagem = `Olá ${nome}, você é Maior de idade.`
+  } else {
+    mensagem = `Olá ${nome}, você é Menor de idade.`
+  }
+  
+  if (nome === 'Thomas Anderson' ||"thomas anderson") {
+    mensagem += 'Você é personagem do filme The Matrix!'
+  }
+
+  titulo.innerHTML = mensagem;
+  subtitulo.innerHTML = mensagem2;
 }
+
